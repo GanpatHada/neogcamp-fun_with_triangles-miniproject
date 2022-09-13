74482d8dafa049b2324f1b912a16b1ad89ce209a
GanpatHada <hadaganpat42@gmail.com>
2022-09-13 20:25:20 +0530

    correction in spelling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-         <p>&copy; copyright 2022 | Ganpat Hada | Birthday Lucky</p>
+         <p>&copy; copyright 2022 | Ganpat Hada | Fun with triangles</p>
          <span>
            <a href="https://www.linkedin.com/in/ganpat-hada-33abb4227/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
            <a href="https://github.com/GanpatHada" target="_blank"><i class="fa-brands fa-github"></i></a>

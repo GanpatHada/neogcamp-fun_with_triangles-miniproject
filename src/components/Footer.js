760d9ee66f17e57d7ab33638a195411f84796130
GanpatHada <hadaganpat42@gmail.com>
2022-09-18 18:44:21 +0530

    added readme

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ const Footer = () => {
     <footer>
          <p>&copy; copyright 2022 | Ganpat Hada | Fun with triangles</p>
          <span>
-           <a rel="noreferrer" href="https://www.linkedin.com/in/ganpat-hada-33abb4227/" target="_blank"><i className="fa-brands fa-linkedin"></i></a>
-           <a rel="noreferrer" href="https://github.com/GanpatHada" target="_blank"><i className="fa-brands fa-github"></i></a>
-           <a href="/" target="_blank"><i className="fa-brands fa-twitter"></i></a>
+           <a href="https://www.linkedin.com/in/ganpat-hada-33abb4227/" target="_blank"><i className="fa-brands fa-linkedin"></i></a>
+           <a href="https://github.com/GanpatHada" target="_blank"><i className="fa-brands fa-github"></i></a>
+           <a href="https://twitter.com/ganpat_hada" target="_blank"><i className="fa-brands fa-twitter"></i></a>
         </span>
     </footer>
   )
